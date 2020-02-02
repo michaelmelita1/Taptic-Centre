a2d25fc9e4b2b06bb4eb153a10532baf0d1996d4
Michael Melita <47737010+michaelmelita1@users.noreply.github.com>
2020-02-02 18:29:41 +1100

    Delete CRose.h

--- CRose.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <AudioToolbox/AudioServices.h>
-
-NSString *tapticLevel = @"0";
-int customFeedbackValue;
-
-// Taptic Engine Feedback generator
-UIImpactFeedbackGenerator *gen;
-
